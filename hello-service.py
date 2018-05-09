d2301b842ae6d82e70d3a96bbe0f6cf551d991f9
dsilvaw <william.dsilva@anz.com>
2018-05-10 09:54:16 +1000

    Reduced the image size by using alpine

--- hello-service.py
+++ hello-service.py
@@ -3,7 +3,7 @@
 from aiohttp import web
 
 async def handle(request):
-    name = request.match_info.get('name', "Jobayer!")
+    name = request.match_info.get('name', "World!")
     text = "Hello, " + name
     print('received request, replying with "{}".'.format(text))
     return web.Response(text=text)
